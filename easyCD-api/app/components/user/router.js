2773f7136f943066221037c47e16895cca188716
Kaio Rodrigues <kaio.rodrigues@clevertech.biz>
2023-04-17 21:04:59 -0300

    Fixing permission of update the user

--- easyCD-api/app/components/user/router.js
+++ easyCD-api/app/components/user/router.js
@@ -18,7 +18,7 @@ exports = module.exports = function initRouter(UserController, Policies) {
   // Update an user
   router.put(
     '/:userId',
-    Policies.JWTAdmin,
+    Policies.JWTLogged,
     UserController.update,
   );
 
