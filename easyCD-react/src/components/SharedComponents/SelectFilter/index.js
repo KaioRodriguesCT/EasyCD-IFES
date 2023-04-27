720c61fc2f289043a01e270bee4090d5a873a518
Kaio Rodrigues <kaio.rodrigues@clevertech.biz>
2023-04-26 22:44:25 -0300

    Adjusting search on select filter

--- easyCD-react/src/components/SharedComponents/SelectFilter/index.js
+++ easyCD-react/src/components/SharedComponents/SelectFilter/index.js
@@ -3,16 +3,21 @@ import { useMemo } from 'react';
 
 import './index.css';
 
-function SelectFilter ({ defaultValue, onChange, data, mapFn, placeholder, allowClear = true }){
-  const options = useMemo(() => mapFn(data),[data, mapFn]);
+function SelectFilter ({ defaultValue, onChange, data, mapFn, placeholder, allowClear = true }) {
+  const options = useMemo(() => mapFn(data), [data, mapFn]);
   return (
     <Select
       defaultValue={defaultValue}
       placeholder={placeholder}
       onChange={onChange}
-      options={options }
+      options={options}
       allowClear={allowClear}
-      style={{ width:'250px', position:'unset !important' }}
+      showSearch
+      filterOption={(inputValue, option) =>
+        String(option?.label).toLowerCase()
+          .includes(String(inputValue).toLowerCase())
+      }
+      style={{ width: '250px', position: 'unset !important' }}
     />
   );
 }
